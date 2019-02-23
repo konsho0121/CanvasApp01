d4ab802324a1edbcc0c3ac44782d9f5657e679c4
Konsho0121 <trt.com@icloud.com>
2019-02-23 16:45:16 +0900

    ThirdCommit

--- app/src/main/java/cosamv/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/cosamv/example/dojo/canvasapp01/MyView.java
@@ -1,6 +1,8 @@
 package cosamv.example.dojo.canvasapp01;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.View;
 
 public class MyView extends View {
@@ -9,6 +11,13 @@ public class MyView extends View {
     // コンストラクタ(初期設定)
     public MyView(Context con) {
         super(con);
+    }
+    //描写の処理
+    @Override
+    protected void  onDraw(Canvas canvas) {
+        //背景
+        canvas.drawColor(Color.DKGRAY);
+
     }
 
 }
